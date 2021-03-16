95713d1843446d599941737de091afcde0a803bf
Xavier Caparros <xcaparros@hotmail.com>
2021-03-16 12:32:09 +0100

    CSS search done

--- src/components/customer/Search/Search.tsx
+++ src/components/customer/Search/Search.tsx
@@ -1,34 +1,47 @@
-import { ReactElement, useEffect, useState } from 'react'
-import './SearchStyle.css'
-import { Link } from 'react-router-dom';
+import { ReactElement, useEffect, useState } from "react";
+import styles from "./Search.module.css";
+import { Link } from "react-router-dom";
 import itemsDB from "../../../lib/item";
 
-
 export default function Search(): ReactElement {
-    useEffect(() => {
-        const fetchItems = async () => {
-          const result = await itemsDB.findAll();
-          if(result.success){
-            setItems(result.body);
-          }
-        };
-        fetchItems();
-      }, []);
-      let [items, setItems] = useState<any>([
-        { title: "", _id: "", description: "", price: "", tags: [] },
-      ]);
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await itemsDB.findAll();
+      if (result.success) {
+        setItems(result.body);
+      }
+    };
+    fetchItems();
+  }, []);
+  let [items, setItems] = useState<any>([
+    { title: "", _id: "", description: "", price: "", tags: [] },
+  ]);
 
-    return (
-        <div className="flowerListContainer">
-            {items.map((item:any)=>(
-            <Link to={`/info/${item._id}`} key={item._id} className="flowerContainer" style={{backgroundImage: `url(${item.img})`}}>
-                <div className="transparent-backgroupd">
-                <p>{item.title}</p>
-                <p>{item.description}</p>
-                <p>Price: {item.price}€</p>
-                </div>
-            </Link>
-            ))}
-        </div>
-    )
+  return (
+    <div className={styles.listContainer}>
+      <h1>Search</h1>
+      {items.map((item: any) => (
+        <Link
+          to={`/info/${item._id}`}
+          key={item._id}
+          className={styles.itemContainer}
+          style={{ backgroundImage: `url(${item.img})` }}
+        >
+          <div className={styles.itemText}>
+            <p>{item.title}</p>
+            {item.discount ? (
+              <>
+                <p style={{ textDecoration: "line-through" }}>
+                  Price: {item.price}€
+                </p>
+                <p>New Price: {item.price}€</p>
+              </>
+            ) : (
+              <p>Price: {item.price}€</p>
+            )}
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 }
