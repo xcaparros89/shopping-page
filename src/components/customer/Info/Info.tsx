e6429ce915d84bbd81cf858b2bc047cbe3f0c13e
Xavier Caparros <xcaparros@hotmail.com>
2021-03-17 15:36:21 +0100

    Updated consumer info component

--- src/components/customer/Info/Info.tsx
+++ src/components/customer/Info/Info.tsx
@@ -1,24 +1,55 @@
-import { ReactElement, useEffect,useState } from 'react'
-import data from '../../../plants.json';
+import { ReactElement, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import itemsDB from "../../../lib/item";
+import { ItemValues } from "../../../interfaces";
 
-export default function Info({match}:any): ReactElement {
-    let [plant, setPlant] = useState({title:'', info:'', price:0, src:''})
-    let id = match.params.id;
-    useEffect(() => {
-        data.forEach(item =>{
-            if(id===item.id){
-                setPlant(item)
-            }});
-    },[id]);
+export default function Info(): ReactElement {
+  let { id } = useParams<{ id: string }>();
+  useEffect(() => {
+    const fetchItem = async () => {
+      const result = await itemsDB.findOne("_id", id);
+      if (result.success) {
+        setItem(result.body);
+      }
+    };
+    fetchItem();
+  }, [id]);
 
-
-    console.log(plant);
-    return (
-        <div>
-            <h1>{plant.title}</h1>
-            <img src={plant.src} alt={plant.title}/>
-            <p>{plant.info}</p>
-            <p>Price: {plant.price}€</p>
-        </div>
-    )
+  let [item, setItem] = useState<ItemValues>({
+    title: "",
+    description: "",
+    img: "",
+    price: 0,
+    discount: 0,
+    tags: [],
+  });
+  return (
+    <div>
+      {item.title ? (
+        <>
+          <h1>{item.title}</h1>
+          <img src={item.img} alt={item.title} />
+          <p>{item.description}</p>
+          {item.discount ? (
+            <>
+                <p style={{textDecoration:'line-through'}}>Price: {item.price}</p>
+              <p>
+                Price:{" "}
+                {item.price - (item.price * item.discount) / 100}
+              </p>
+              <p>Discount:{item.discount}%</p>
+            </>
+          ) : (
+            <p>Price: {item.price}€</p>
+          )}
+          <p>Categories:</p>
+          {item.tags.map(categ=>
+              <li>{categ}</li>
+          )}
+        </>
+      ) : (
+        <p>No item found</p>
+      )}
+    </div>
+  );
 }
