3ffa985fca3654944e3c3183319ce5568708470e
Xavier Caparros <xcaparros@hotmail.com>
2021-03-23 12:22:35 +0100

    Fixed tests of components with useContext

--- src/components/customer/Header/Header.test.js
+++ src/components/customer/Header/Header.test.js
@@ -1,25 +1,17 @@
-import React, {useState} from "react";
-import { render} from "@testing-library/react";
+import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Header from "./Header";
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter } from "react-router-dom";
+import { UserProvider } from "../../../lib/AuthProvider";
 
-
-
-// const UserContext = React.createContext()
-// test("<Header /> renders without crashing", () => {
-//   const [user, setUser] = useState({
-//     userInfo: null,
-//     isCustomer: false,
-//     isAdmin: false,
-//   });
-//   const div = document.createElement("div");
-//   render(
-//       <MemoryRouter>
-//       <UserContext.Provider value = {[user, setUser]}>
-//     <Header />
-//       </UserContext.Provider>
-//       </MemoryRouter>,
-//     div
-//   );
-// });
+test("<Header /> renders without crashing", () => {
+  const div = document.createElement("div");
+  render(
+    <MemoryRouter>
+      <UserProvider>
+        <Header />
+      </UserProvider>
+    </MemoryRouter>,
+    div
+  );
+});
--- src/components/customer/Login/Login.test.js
+++ src/components/customer/Login/Login.test.js
@@ -2,13 +2,16 @@ import { render} from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Login from "./Login";
 import { MemoryRouter } from 'react-router-dom';
+import { UserProvider } from "../../../lib/AuthProvider";
 
-// test("<Login /> renders without crashing", () => {
-//   const div = document.createElement("div");
-//   render(
-//       <MemoryRouter>
-//     <Login />
-//       </MemoryRouter>,
-//     div
-//   );
-// });
+test("<Login /> renders without crashing", () => {
+  const div = document.createElement("div");
+  render(
+      <MemoryRouter>
+      <UserProvider>
+    <Login />
+      </UserProvider>
+      </MemoryRouter>,
+    div
+  );
+});
